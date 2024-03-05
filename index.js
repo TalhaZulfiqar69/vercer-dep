81e748d593c348659020712f442b8959d8f15253
Talha Zee <macintosh@Talha-Zee.local>
2024-03-05 23:43:47 +0100

    initial commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/hello", (req, res) => res.json("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
